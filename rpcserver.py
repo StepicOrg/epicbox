1c301b76226625b49bc7f413e22edf270c35455d
Pavel Sviderski <pavel@psviderski.name>
2015-02-26 15:49:53 +0300

    add rpcserver.py start script, update .gitignore

---
+++ rpcserver.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import logging
+import signal
+import sys
+
+import structlog
+
+import epicbox
+
+from functools import partial
+
+from epicbox import rpc, settings
+
+
+logger = structlog.get_logger()
+
+
+def configure():
+    logging.basicConfig(level=logging.INFO)1
+
+    if isinstance(settings.PROFILES, (tuple, list)):
+        profiles = settings.PROFILES
+    elif isinstance(settings.PROFILES, dict):
+        profiles = [epicbox.Profile(name, **profile_kwargs)
+                    for name, profile_kwargs in settings.PROFILES.items()]
+    else:
+        raise KeyError("'PROFILES' setting should be list of dict")
+
+    epicbox.configure(
+        profiles=profiles,
+        docker_url=settings.DOCKER_URL,
+        base_workdir=settings.BASE_WORKDIR,
+        selinux_enforced=settings.SELINUX_ENFORCED,
+    )
+
+
+def register_shutdown_handler(handler):
+    """Register a handler that will be called on process termination."""
+
+    def _signal_handler(signum, frame):
+        logger.info("Signal handler called with signal", signum=signum)
+        handler()
+        sys.exit(0)
+
+    for sig in [signal.SIGTERM, signal.SIGINT]:
+        signal.signal(sig, _signal_handler)
+
+
+def stop_server(rpc_server):
+    """Attempt to stop the RPC server gracefully."""
+    # TODO: configure logging
+    logger.info("Stopping RPC server...")
+    try:
+        rpc_server.stop()
+        rpc_server.wait()
+    except Exception:
+        pass
+
+
+def main():
+    configure()
+    rpc_server = rpc.get_server(settings.RPC_TRANSPORT_URL)
+    shutdown_handler = partial(stop_server, rpc_server)
+    register_shutdown_handler(shutdown_handler)
+    logger.info("Starting EpicBox RPC server...")
+    rpc_server.start()
+    rpc_server.wait()
+
+
+if __name__ == '__main__':
+    main()
