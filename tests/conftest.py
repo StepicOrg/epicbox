9b527f2259b1ca36c9a5592e70546085accedd40
Pavel Sviderski <pavel@psviderski.name>
2015-03-07 02:16:12 +0300

    add functional tests for rpc client

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,16 @@ import pytest
 
 import structlog
 
+from epicbox.rpcapi import EpicBoxAPI
+
 
 def pytest_addoption(parser):
     parser.addoption('--docker-url', action='store', default=None,
                      help="Use this url to connect to a Docker backend server")
     parser.addoption('--selinux', action='store_true', default=False,
                      help="Use this option if SELinux policy is enforced")
+    parser.addoption('--rpc-url', action='store', default=None,
+                     help="Use real RPC server transport for functional tests")
 
 
 @pytest.fixture
@@ -50,3 +54,15 @@ def configure(profile, docker_url, selinux_enforced):
         ],
         logger_factory=structlog.PrintLoggerFactory(),
     )
+
+
+@pytest.fixture
+def rpc_transport_url(request):
+    return request.config.getoption('rpc_url')
+
+
+@pytest.fixture
+def rpcepicbox(rpc_transport_url):
+    if rpc_transport_url:
+        return EpicBoxAPI(rpc_transport_url)
+    return EpicBoxAPI(None, fake_server=True)
---
+++ tests/test_rpc_functional.py
@@ -0,0 +1,35 @@
+import pytest
+
+from unittest.mock import ANY
+
+
+def test_run_python(profile, rpcepicbox):
+    result = rpcepicbox.run(profile.name, 'python3 -c "print(42)"')
+
+    expected_result = {
+        'exit_code': 0,
+        'stdout': b'42\n',
+        'stderr': b'',
+        'duration': ANY,
+        'timeout': False,
+        'oom_killed': False,
+    }
+    assert result == expected_result
+    assert result['duration'] > 0
+
+
+def test_run_raise_exception(rpcepicbox):
+    with pytest.raises(ValueError):
+        rpcepicbox.run('unknown', 'true')
+
+
+def test_run_upload_files(skip_if_remote_docker, profile, rpcepicbox):
+    files = [
+        {'name': 'main.py', 'content': b'print(open("file.txt").read())'},
+        {'name': 'file.txt', 'content': b'Data in file.txt'},
+    ]
+
+    result = rpcepicbox.run(profile.name, 'python3 main.py', files=files)
+
+    assert result['exit_code'] == 0
+    assert result['stdout'] == b'Data in file.txt\n'
